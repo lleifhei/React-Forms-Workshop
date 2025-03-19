65c614fe63a2da6f693d5bec3b3f9a101c5d81c8
lleifhei <loganleifheit@gmail.com>
2025-03-18 22:36:29 -0400

    added components

---
+++ FormsWorkshop/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <SignUpForm />
+      <Authenticate />
+
+    </>
+  )
+}
+
+export default App
