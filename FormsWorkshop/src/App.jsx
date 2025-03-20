c9328efe371bf3ccf60d0a7aaea3434253d08a3c
lleifhei <loganleifheit@gmail.com>
2025-03-20 17:18:53 -0400

    authentication button

--- FormsWorkshop/src/App.jsx
+++ FormsWorkshop/src/App.jsx
@@ -4,10 +4,12 @@ import SignUpForm from "./components/SignUpForm";
 import Authenticate from "./components/Authenticate";
 
 function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <>
-      <Authenticate />
-      <SignUpForm />
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
     </>
   );
 }
--- FormsWorkshop/src/components/Authenticate.jsx
+++ FormsWorkshop/src/components/Authenticate.jsx
@@ -1,3 +1,25 @@
-export default function Authenticate() {
-    return <h2>Authenticate!</h2>;
-  }
+export default function Authenticate({ token }) {
+  token ? console.log("Authenticated!") : console.log("Not authenticated!");
+  return (
+    <>
+      <h2>Authenticate!</h2>
+      <button
+        onClick={() => {
+          //function to send GET request to authenticate endpoint
+          fetch("https://fsa-jwt-practice.herokuapp.com/authenticate", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err));
+        }}
+      >
+        Authenticate
+      </button>
+    </>
+  );
+}
